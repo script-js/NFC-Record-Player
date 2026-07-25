5062b2bc7015460dc7128c4c8c24e31968ddb0e2
Jakob <134544514+script-js@users.noreply.github.com>
2026-07-25 16:22:37 -0700

    switch error logging and remove try

--- nfc.js
+++ nfc.js
@@ -1,24 +1,16 @@
-try {
-    const ndef = new NDEFReader();
-} catch (err) {
-    alert(err)
-}
+const ndef = new NDEFReader();
 
 async function read() {
-    try {
-        await ndef.scan();
-        console.log("> Scan started");
-        ndef.addEventListener("readingerror", () => {
-            console.log("Argh! Cannot read data from the NFC tag. Try another one?");
-        });
+    await ndef.scan();
+    console.log("> Scan started");
+    ndef.addEventListener("readingerror", () => {
+        console.log("Argh! Cannot read data from the NFC tag. Try another one?");
+    });
 
-        ndef.addEventListener("reading", ({ message, serialNumber }) => {
-            console.log(message)
-            if (confirm("Would you like to write?")) {
-                ndef.write("test")
-            }
-        });
-    } catch (error) {
-        console.log("Argh! " + error);
-    }
+    ndef.addEventListener("reading", ({ message, serialNumber }) => {
+        console.log(message)
+        if (confirm("Would you like to write?")) {
+            ndef.write("test")
+        }
+    });
 }
