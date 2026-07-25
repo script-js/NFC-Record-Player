d7be6ca959467bfaefcfa1d4dd3ffae4b48d51bc
Jakob <134544514+script-js@users.noreply.github.com>
2026-07-24 20:35:06 -0700

    initial

---
+++ nfc.js
@@ -0,0 +1,19 @@
+function read() {
+    try {
+        const ndef = new NDEFReader();
+        await ndef.scan();
+        console.log("> Scan started");
+
+        ndef.addEventListener("readingerror", () => {
+            console.log("Argh! Cannot read data from the NFC tag. Try another one?");
+        });
+
+        ndef.addEventListener("reading", ({ message, serialNumber }) => {
+            console.log(`> Serial Number: ${serialNumber}`);
+            console.log(`> Records: (${message.records.length})`);
+        });
+    } catch (error) {
+        console.log("Argh! " + error);
+    }
+}
+read()
