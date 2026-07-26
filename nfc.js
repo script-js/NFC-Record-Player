8995cdbca559d0bff19d4e8f72cd474bdc61e1c0
Jakob <134544514+script-js@users.noreply.github.com>
2026-07-25 17:02:09 -0700

    encoding

--- nfc.js
+++ nfc.js
@@ -12,20 +12,28 @@ async function read() {
         if (!writing) {
             console.log(message.records)
             var record = message.records[message.records.length - 1]
-            if (record.recordType === "text") {
+            if (record && record.recordType === "text") {
                 var decoder = new TextDecoder(record.encoding);
 
                 var textData = decoder.decode(record.data);
 
                 console.log(`Text content: ${textData}`);
                 console.log(`Language code: ${record.lang}`);
-                alert(textData)
+                if (textData.includes("NFCRECORDPLAYER:")) {
+                    var tagjson = JSON.parse(textData.replace("NFCRECORDPLAYER:",""))
+                    alert("Provider: " + tagjson.provider + "\nURI: " + tagjson.uri)
+                } else {
+                    alert("Invalid NFC Tag")
+                }
+            } else {
+                alert("Invalid NFC Tag")
             }
         }
     });
 }
 
-function writeTag(text) {
+function writeTag(provider, uri) {
+    var text = "NFCRECORDPLAYER:" + JSON.stringify({provider, uri})
     writing = true;
     console.log("Waiting to write...")
     ndef.addEventListener("reading", ({ message, serialNumber }) => {
